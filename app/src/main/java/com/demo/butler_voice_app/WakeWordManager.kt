39db87ecfce2da42182050584f8552c56ff66971
developer-risiga <developer@risiga.com>
2026-03-20 15:41:22 +0530

    Fix wake word (Hey Butler custom model)

--- app/src/main/java/com/demo/butler_voice_app/WakeWordManager.kt
+++ app/src/main/java/com/demo/butler_voice_app/WakeWordManager.kt
@@ -10,7 +10,7 @@ class WakeWordManager(
 
     private val porcupineManager = PorcupineManager.Builder()
         .setAccessKey("EX1Uf3pS1ekA7wReSzb3BaOCHyXmFdGg4ZHCMwRMceqUbsnAD31btw==")
-        .setKeywordPath("hey-butler.ppn") // ✅ CUSTOM FILE
+        .setKeywordPath("hey_butler.ppn") // ✅ CUSTOM FILE
         .setSensitivity(0.7f)
         .build(context) { _ ->
             onWake()
